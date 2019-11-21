cd9824a3af4b9ff421a1b3cbce14f7ac3685a508
wojtekwojtek340 <51856023+wojtekwojtek340@users.noreply.github.com>
2019-11-21 17:16:27 +0100

    WojciechWojtowiczLab1
    Projket wykonany na labolatorium numer 1

---
+++ WojciechWojtowiczLab1/WojciechWojtowiczLabb1/FormAbout.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WojciechWojtowiczLabb1
+{
+    public partial class FormAbout : Form
+    {
+        public string formName = "Wojciech Wojtowicz";
+
+        public FormAbout()
+        {
+            InitializeComponent();
+            labelName.Text = formName;
+        }
+
+        private void FormAbout_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
