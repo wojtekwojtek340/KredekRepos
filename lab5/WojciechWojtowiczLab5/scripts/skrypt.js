40808119ce36f154614870a01ff02a6f2ea6261e
wojtekwojtek340 <51856023+wojtekwojtek340@users.noreply.github.com>
2020-01-01 17:19:02 +0100

    lab5

---
+++ lab5/WojciechWojtowiczLab5/scripts/skrypt.js
@@ -0,0 +1,75 @@
+﻿//zmienna numer uzywana do ładowanie obrazka na strone
+let number = 1;
+
+//funkcja obsługująca zegar na stronie
+function clock() {
+    var today = new Date();
+    var day = today.getDate();
+    var month = today.getMonth() + 1;
+    var year = today.getFullYear();
+    var hour = today.getHours();
+    if (hour < 10) hour = "0" + hour;
+    var minute = today.getMinutes();
+    if (minute < 10) minute = "0" + minute;
+    var secound = today.getSeconds();
+    if (secound < 10) secound = "0" + secound;
+
+    document.getElementById("clock").innerHTML =
+        day + "/" + month + "/" + year + "|" + hour + ":" + minute + ":" + secound;
+
+    setTimeout("clock()", 1000)
+}
+
+//funckja służąca do przełączania pomiedzy róznymi obrazkami
+function characterInfo(id) {
+
+    if (id == "rick") {
+        let file = "<img src=\"../pictures/" + id + ".jpg\"height=300 width=220/>";        
+        document.getElementById("content2").innerHTML = "Rick jest wysokim, chudym starcem. Ma długie nogi i ramiona i jest bardzo szczupły. Posiada bladą cerę i jasne, szaroniebieskie spiczaste włosy z łysą plamą z tyłu głowy oraz monobrew. Jego twarz jest pomarszczona. Widoczne są na niej liczne zmarszczki oraz wory pod oczami. Od czasu do czasu na jego brodzie widoczna jest zielona plama, która pojawia się głównie wtedy, gdy jest pijany lub wymiotuje. Rick wypowiada się w chaotyczny, bełkotliwy sposób. Gdy mówi, często przerywa swoją wypowiedź, by beknąć czy czknąć, co jest bezpośrednim wynikiem tego, iż jest pijany. Naukowiec nosi jasnoniebieski golf, brązowe spodnie spięte ciemnobrązowym paskiem z żółtą klamrą, czarne buty oraz biały fartuch laboratoryjny.";
+        document.getElementById("picture").innerHTML = file;        
+    }
+    else if (id == "morty") { 
+        let file = "<img src=\"../pictures/" + id + ".jpg \"height=300 width=220/>";        
+        document.getElementById("content2").innerHTML = "Morty jest nastoletnim chłopakiem o krótkich, brązowych i starannie zaczesanych włosach oraz owalnej głowie, która wyróżnia go spośród innych postaci w serialu. Morty nosi żółtą podkoszulkę, niebieskie spodnie i białe buty. Gdy mówi, często się jąka, a jego głos stale się łamie ze względu na skutki okresu dojrzewania.";
+        document.getElementById("picture").innerHTML = file;
+    }
+}
+
+//funckja obsługująca zanikanie obrazka przy uzyciu Jquery
+function hide() {
+    $("#picture2").fadeOut(500);
+}
+
+//funckja obsłgująca zmiene obrazka
+function changePicture() {
+
+    let file = "<img src=\"../pictures/" + number + ".jpg\"width=980/>";
+    document.getElementById("picture2").innerHTML = file;
+    number++;  
+
+    $("#picture2").fadeIn(500);
+    setTimeout("changePicture()", 5000);
+    setTimeout("hide()", 4500);
+    if (number == 5) {
+        number = 1;
+    }
+}
+
+//funkcja obsługująca dodawanie nazwy osoby która przegląda stronę
+function addName() {
+    name = document.getElementById("box").value;
+    localStorage.setItem('Imie', name);
+    load();
+}
+
+//funkcja ładująca nawę uzytkownika który przegląda stronę
+function load() {
+
+    if (localStorage.getItem('Imie') != null) {
+        document.getElementById("myName").innerHTML = "Przeglądasz jako: " + localStorage.getItem('Imie');
+    }
+    else {
+        document.getElementById("myName").innerHTML = "Przeglądasz jako anonim";
+    }
+
+}
---
+++ lab5/WojciechWojtowiczLab5/scripts/skrypt2.js
@@ -0,0 +1,107 @@
+﻿//odwołąnie do obiektu z treścią zgadywanej postaci
+const heroText = document.getElementById('heroText');
+//odwołąnie do obiektów z klasą box
+const boxes = document.querySelectorAll('.box');
+//odwołanie do obiektu message
+const message = document.getElementById('message')
+
+//wartość przełączająca miedzy trybem grania i koncem gry
+let isPlaying = true;
+//odwołanie do instancji instrukcji setInterval
+let countDown;
+//licznik czasu odliczania do następnej rundy
+let timer = 5;
+//tablica przechowująca bohaterów serialu
+let hero = generateRandomHero();
+//wybrany boharer
+let selectedHero;
+
+//funkcja ustawiająca obrazek
+function setPicture() {
+
+    for (let i = 1; i <= 3; i++) {
+        let file = "<img src=\"../pictures/gra/" + i + ".jpg\"height=200 width=200/>";
+        boxes[i-1].innerHTML = file;
+    }
+}
+
+//funckja dodająca id obazka to tablicy i zwracająca tą tablicę
+function generateRandomHero() {
+    let arr = [];
+    for (let i = 0; i < boxes.length; i++) {
+        arr.push(i+1);
+    }
+    return arr;
+}
+
+//funckja losująca ktorego bohatera należy zgadnąć
+function heroName() {
+    selectedHero = Math.floor(Math.random() * 3);
+
+    if (hero[selectedHero] == 1) {
+        document.getElementById("heroText").innerHTML = "Rick";
+    }
+    else if (hero[selectedHero] == 2) {
+        document.getElementById("heroText").innerHTML = "Morty";
+    }
+    else if (hero[selectedHero] == 3) {
+        document.getElementById("heroText").innerHTML = "Jerry";
+    }
+    selectedHero = hero[selectedHero];
+    setPicture();
+}
+
+//funkcja ustawiająca obrazki na obrazek wygranego bohatera
+function setWinner(id) {
+    for (let i = 1; i <= 3; i++) {
+        let file = "<img src=\"../pictures/gra/" + id + ".jpg\"height=200 width=200/>";
+        boxes[i - 1].innerHTML = file;
+        boxes[i - 1].style.opacity = '1';
+    }
+}
+
+//funckja resetująca zakończoną grę
+function resetGame() {
+    clearInterval(countDown);
+    isPlaying = true;
+    timer = 5;
+    message.textContent = 'Wybierz odpowiednią postać';
+    heroName();
+
+}
+
+// iteracja dla wszystkich kwadratów
+for (let i = 0; i < boxes.length; i++) {
+    //dodawanie event listenera dla każedgo kwadrata na zdarzenie 'click'
+    boxes[i].addEventListener('click', function (e) {
+        // sprawdzenie trybu gry
+        if (isPlaying) {
+            // sprawdzenie czy kliknięty bohater jest tym wybranym
+            if (i+1 === selectedHero) {
+                //przełaczenie trybu gry
+                isPlaying = false;
+
+                // komunikat do gracza
+                message.textContent = 'BRAWO!';
+
+                // ustwienie zgadniętej postaci
+                setWinner(selectedHero);
+
+                // odroczenie funkcji resetGame na 7 sekund
+                setTimeout(resetGame, 7000);
+
+                // ustawinie interwału na każdą sekundę
+                countDown = setInterval(function () {
+                    // uruchomienie licznika
+                    heroText.textContent = `Rozpoczęcie następnej rundy za ${timer} sekund`;
+                    timer--;
+                }, 1000);
+            } else {
+                // ukrycie klikniętego kwadrata
+                boxes[i].style.opacity = '0';
+                // komunikat do gracza
+                message.textContent = 'Próbuj dalej';
+            }
+        }
+    });
+}
