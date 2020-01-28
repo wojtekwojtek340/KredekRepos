0b2d6becdd5f69bc27991e3c2b0a3c1352c725ef
wojtekwojtek340 <51856023+wojtekwojtek340@users.noreply.github.com>
2020-01-28 15:13:21 +0100

    Poprawki

--- lab3/Lab3/WojciechWojtowiczLab3Zadanie/WojciechWojtowiczLab3Zadanie.Tests/PlayerShould.cs
+++ lab3/Lab3/WojciechWojtowiczLab3Zadanie/WojciechWojtowiczLab3Zadanie.Tests/PlayerShould.cs
@@ -1,5 +1,4 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
@@ -8,6 +7,9 @@ namespace WojciechWojtowiczLab3Zadanie.Tests
 {
     public class PlayerShould
     {
+        /// <summary>
+        /// test do sprawdzenia czy ustawiony parametr jest równy pobranemu
+        /// </summary>
         [Fact]
         public void CalculatePlayerMoney()
         {
@@ -15,7 +17,46 @@ namespace WojciechWojtowiczLab3Zadanie.Tests
 
             sut.SetMoney(50);
 
-            Assert.Equals(50, sut.GetMoney());
+            Assert.Equal(50, sut.GetMoney());
+        }
+
+        /// <summary>
+        /// test do sprawdzenia czy ustawiony parametr jest różny jakiegoś innego
+        /// </summary>
+        [Fact]
+        public void CalculatePlayerMoney2()
+        {
+            Player sut = new Player();
+
+            sut.SetMoney(50);
+
+            Assert.NotEqual(100, sut.GetMoney());
+        }
+
+        /// <summary>
+        ///  test do sprawdzenia czy ustawiony parametr jest równy pobranemu
+        /// </summary>
+        [Fact]
+        public void CalculatePlayerHonor()
+        {
+            Player sut = new Player();
+
+            sut.SetHonor(100);
+
+            Assert.Equal(100, sut.GetHonor());
+        }
+
+        /// <summary>
+        /// test do sprawdzenia czy ustawiony parametr jest różny od jakiegoś innego
+        /// </summary>
+        [Fact]
+        public void CalculatePlayerHonor2()
+        {
+            Player sut = new Player();
+
+            sut.SetHonor(100);
+
+            Assert.NotEqual(123, sut.GetHonor());
         }
     }
 }
